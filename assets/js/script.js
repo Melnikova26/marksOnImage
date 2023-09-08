f2a444926ffb2c39c3a0ff4a91b5781058d27a24
Melnikova26 <mmelnekowa@gmail.com>
2023-09-08 09:59:47 +0500

    feature: добавлен script.js, также сделан адаптив для ширины от 1280px до 1920px

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const btns = document.querySelectorAll('.main__btn');
+    console.log(window.innerHeight, window.innerWidth);
+    document.addEventListener('click', function(e){
+        const targetElement = e.target.closest('.main__btn') || e.target.closest('.main');
+        if(targetElement.classList.contains('main__btn')){
+            const btnMinus = targetElement.querySelector('.main__minus'),
+                 btnPlus = targetElement.querySelector('.main__plus');
+                btnMinus.classList.toggle('hidden');
+                btnPlus.classList.toggle('hidden');
+        } else if (targetElement.classList.contains('main')){
+            btns.forEach(btn => {
+                const btnMinus = btn.querySelector('.main__minus'),
+                btnPlus = btn.querySelector('.main__plus');
+                btnMinus.classList.add('hidden');
+                btnPlus.classList.remove('hidden');
+            })
+        }
+    })    
+});
